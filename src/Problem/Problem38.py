80b808ab6446c5414ab772ab033bf37807070434
Shane Auckland <shane.auckland@gmail.com>
2013-12-17 11:34:42 +0000

    Add problem 38

---
+++ src/Problem/Problem38.py
@@ -0,0 +1,39 @@
+#!/usr/bin/python
+
+"""
+    Project Eular - Problem 38
+    
+    Take the number 192 and multiply it by each of 1, 2, and 3:
+
+    192 x 1 = 192
+    192 x 2 = 384
+    192 x 3 = 576
+
+    By concatenating each product we get the 1 to 9 pandigital, 192384576. We will call 192384576 the concatenated product of 192 and (1,2,3)
+
+    The same can be achieved by starting with 9 and multiplying by 1, 2, 3, 4, and 5, giving the pandigital, 918273645, which is the concatenated product of 9 and (1,2,3,4,5).
+
+    What is the largest 1 to 9 pandigital 9-digit number that can be formed as the concatenated product of an integer with (1,2, ... , n) where n > 1?
+
+    @package Problem
+    @author Shane Auckland <shane.auckland@gmail.con>
+"""
+import math
+
+def isPandigital(num):
+    return sorted(num) == ['1', '2', '3', '4', '5', '6', '7', '8', '9']
+
+largest = 0
+i = 2
+while i < 10000:
+    print i, largest
+    n = 2
+    conc = str(i)
+    while len(conc) < 10:
+        conc += str(i*n)
+        if isPandigital(conc) and int(conc) > largest:
+            largest = int(conc)
+        n += 1
+    i += 1
+
+print largest
